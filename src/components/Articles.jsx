6db0a04b313ad6da9a81d02276f1d12bac81ee4f
adunners <alexdunne14@gmail.com>
2024-02-07 14:20:41 +0000

    add updates to comments

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -8,11 +8,13 @@ export default function Articles() {
   const [allArticles, setAllArticles] = useState([]);
   const [articleId, setArticleId] = useState("");
   const [listSingleArticle, setListSingleArticle] = useState(false);
+  const [isLoading, setIsLoading] = useState(true)
 
 
   useEffect(() => {
     getAllArticles().then((everyArticle) => {
       setAllArticles(everyArticle);
+      setIsLoading(false)
     });
   }, []);
 
@@ -22,17 +24,26 @@ export default function Articles() {
     setArticleId("");
   }
 
+const article =  allArticles.find((article) => {return article.article_id === Number(articleId)})
+
+
   return (
     <>
       <h2 className="articles-h2">Articles </h2>
      <SearchArticle articleId={articleId} setArticleId={setArticleId} setListSingleArticle={setListSingleArticle} allArticles={allArticles}/>
+    {isLoading ? <p>loading...</p>: 
       <section className="articles-section">
         {listSingleArticle ? (
           <>
+          <div className="articles-search-grid">
             <h3>Your Article Search</h3>
+            <div className="articles-link-container">
             <Link className="articles-link" to={articleId}>
-              Article {articleId}
+             
+               <p>Article {articleId}</p> 
+               <img className="articles-img-1" src={article.article_img_url} alt="image from the selected article"/>
             </Link>
+            </div>
             <button
               className="articles-button"
               type="submit"
@@ -41,6 +52,7 @@ export default function Articles() {
             >
               Back
             </button>
+            </div>
           </>
         ) : (
           allArticles.map((article) => {
@@ -49,7 +61,7 @@ export default function Articles() {
                 <header>
                   <h3 className="articles-h3">{article.title}</h3>
                   <p className="articles-p">Topic: {article.topic}</p>
-                  <p className="articles-p">Author: {article.author}</p>
+                  <p className="articles-p">Author: {article.artic}</p>
                   <p className="articles-p">
                     Published:
                     {new Date(article.created_at).toLocaleDateString()}
@@ -70,7 +82,8 @@ export default function Articles() {
             );
           })
         )}
-      </section>
+       </section>
+    }
     </>
   );
 }
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,30 +1,49 @@
 import { useEffect, useState } from "react";
 import { getArticleIdComments } from "../utils/api";
-import "../css/Comments.css"
+import "../css/Comments.css";
 
+export default function Comments({ articleId }) {
+  const [commentsById, setCommentsById] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-export default function Comments({articleId}) {
-    const [commentsById, setCommentsById] = useState([])
+  useEffect(() => {
+    getArticleIdComments(articleId).then((comments) => {
+      setCommentsById(comments);
+      setIsLoading(false);
+    });
+  }, []);
 
-    useEffect(() => {
-        getArticleIdComments(articleId).then((comments) => {
-            setCommentsById(comments)
-        })
-    },[])
-    
-    return (
+  return (
+    <>
+      {isLoading ? (
+        <p>loading...</p>
+      ) : (
         <>
-        <h3 className="comments-h3">Comments</h3>
-        <div className="comments-comment">
-            {commentsById.map((comment) => {
-                return <article className="comments-article" key={comment.comment_id} >
-                    <p className="comments-content">Comment:{" "}{comment.body}</p>
-                    <p className="comments-content">Votes:{" "}{comment.votes}</p>
-                    <p className="comments-author">Auhtor:{" "}{comment.author}</p>
-                    <p className="comments-time">Published:{" "} {new Date(comment.created_at).toLocaleDateString()}</p>
-                </article>
-            })}
-        </div>
+          <h3 className="comments-h3">Comments</h3>
+          <div className="comments-comment">
+            {commentsById.length === 0 ? (
+              <p>no comments for this article</p>
+            ) : (
+              commentsById.map((comment) => {
+                return (
+                  <article
+                    className="comments-article"
+                    key={comment.comment_id}
+                  >
+                    <p className="comments-content">Comment: {comment.body}</p>
+                    <p className="comments-content">Votes: {comment.votes}</p>
+                    <p className="comments-author">Auhtor: {comment.author}</p>
+                    <p className="comments-time">
+                      Published:{" "}
+                      {new Date(comment.created_at).toLocaleDateString()}
+                    </p>
+                  </article>
+                );
+              })
+            )}
+          </div>
         </>
-    )
+      )}
+    </>
+  );
 }
